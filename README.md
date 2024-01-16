2160beb41418056a52a1ab310c3492dc23cec42e
ntkunz <33107555+ntkunz@users.noreply.github.com>
2024-01-16 12:31:48 -0800

    add portfolio-simple to repo

---
+++ README.md
@@ -0,0 +1,9 @@
+Nicholas Kunz's Web Development Portfolio
+
+A simple portfolio site created with html, css, and javascript.
+
+It has some dom manipulation to display data from a json, rather than hardcoding it.
+
+It also utilizes some simple animations from https://animate.style/
+
+Thanks a lot and I hope you enjoy.
---
+++ main.js
@@ -0,0 +1,110 @@
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+fetch('./data.json')
+    .then((response) => response.json())
+    .then((data) => {
+        const portfolioContainer = document.getElementById('main')
+
+        data.forEach((section) => {
+            Object.entries(section).forEach(([key, items]) => {
+                const sectionTitle = document.createElement('h2')
+                sectionTitle.textContent = capitalizeFirstLetter(key)
+                sectionTitle.id = key
+                portfolioContainer.appendChild(sectionTitle)
+
+                items.forEach((item) => {
+                    const projectContainer = document.createElement('div')
+                    projectContainer.classList.add('project')
+
+                    const title = document.createElement('h3')
+                    title.textContent = capitalizeFirstLetter(item.title)
+                    projectContainer.appendChild(title)
+
+                    if (item.description) {
+                        item.description.forEach((desc) => {
+                            const description = document.createElement('p')
+                            description.textContent = desc
+                            projectContainer.appendChild(description)
+                        })
+                    }
+
+                    if (item.url) {
+                        const url = document.createElement('a')
+                        url.href = item.url
+                        url.textContent = 'View Live Site'
+                        url.target = '_blank'
+                        url.rel = 'noopener noreferrer'
+                        projectContainer.appendChild(url)
+                    }
+
+                    const imageContainer = document.createElement('div')
+                    imageContainer.classList.add('image-container')
+                    projectContainer.appendChild(imageContainer)
+
+                    item.images &&
+                        item.images.forEach((imageUrl) => {
+                            const image = document.createElement('img')
+                            image.src = './images/' + imageUrl
+                            imageContainer.appendChild(image)
+                        })
+
+                    if (item.frontend) {
+                        const frontend = document.createElement('a')
+                        frontend.href = item.frontend
+                        frontend.textContent = 'Frontend Repo'
+                        frontend.target = '_blank'
+                        frontend.rel = 'noopener noreferrer'
+                        projectContainer.appendChild(frontend)
+                    }
+
+                    if (item.backend) {
+                        const backend = document.createElement('a')
+                        backend.href = item.backend
+                        backend.textContent = 'Backend Repo'
+                        backend.target = '_blank'
+                        backend.rel = 'noopener noreferrer'
+                        projectContainer.appendChild(backend)
+                    }
+
+                    if (item.icons) {
+                        const icons = document.createElement('div')
+                        item.icons.forEach((icon) => {
+                            const iconImg = document.createElement('img')
+                            iconImg.classList.add('icon')
+                            iconImg.src = './images/icons/' + icon
+                            icons.appendChild(iconImg)
+                        })
+                        projectContainer.appendChild(icons)
+                    }
+
+                    if (item.email) {
+                        const emailLink = document.createElement('a')
+                        emailLink.href = 'mailto:' + item.email
+                        emailLink.textContent = item.email
+                        projectContainer.appendChild(emailLink)
+                    }
+
+                    if (item.linkedIn) {
+                        const linkedInLink = document.createElement('a')
+                        linkedInLink.href = item.linkedIn
+                        linkedInLink.target = '_blank'
+                        linkedInLink.textContent = item.linkedIn
+                        projectContainer.appendChild(linkedInLink)
+                    }
+
+                    portfolioContainer.classList.add(
+                        'animate__animated',
+                        'animate__fadeInUp',
+                        'animate__delay-3s'
+                    )
+                    portfolioContainer.appendChild(projectContainer)
+                })
+            })
+        })
+    })
+    .catch((error) => {
+        console.error('Error:', error)
+        portfolioContainer.textContent = 'Error: ' + error
+    })
